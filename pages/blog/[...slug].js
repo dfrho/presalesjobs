5b55f6df190b381c17802be44e40d2d82a15a2b8
David Rhodes <david@espressocode.tech>
2023-03-19 19:35:42 -0500

    trying animation

--- pages/blog/[...slug].js
+++ pages/blog/[...slug].js
@@ -9,16 +9,58 @@ import copyLightIcon from '../../public/static/icons/copylight.svg'
 import { useTheme } from 'next-themes'
 import { useEffect } from 'react'
 
+const CopyButton = styled.button`
+  position: relative;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  background-color: green;
+  border: 1px solid gray;
+  padding: 8px 16px;
+  border-radius: 4px;
+  margin-left: 16px;
+  cursor: pointer;
+  color: #fff;
+  outline: none;
+  &:hover {
+    background-color: darkgreen;
+  }
+  &:focus:not(:active)::before {
+    content: '';
+    position: absolute;
+    top: -4px;
+    left: -4px;
+    right: -4px;
+    bottom: -4px;
+    border: 2px solid white;
+    border-radius: 4px;
+  }
+  &.rotate {
+    animation: rotate 1s ease-in-out;
+  }
+`
+
+const CopyButtonDiv = styled.div`
+  display: inline-flex;
+  align-items: center;
+`
+
 const CopyIcon = styled.svg`
   width: 16px;
   height: 16px;
-  color: #fff;
   margin-right: 8px;
-
-  &:hover {
-    cursor: pointer;
-  }
 `
+
+// const CopyIcon = styled.svg`
+//   width: 16px;
+//   height: 16px;
+//   color: #fff;
+//   margin-right: 8px;
+
+//   &:hover {
+//     cursor: pointer;
+//   }
+// `
 
 const Wrapper = styled.article`
   max-width: 700px;
@@ -48,26 +90,26 @@ const ApplyLink = styled.a`
     background-color: darkgreen;
   }
 `
-const CopyButton = styled.div`
-  display: inline-block;
-  background-color: green;
-  border: 1px solid gray;
-  padding: 8px 16px;
-  border-radius: 4px;
-  margin-left: 16px;
-  cursor: pointer;
-  color: #fff;
-  outline: none;
-  &:hover {
-    background-color: darkgreen;
-  }
-`
+// const CopyButton = styled.div`
+//   display: inline-block;
+//   background-color: green;
+//   border: 1px solid gray;
+//   padding: 8px 16px;
+//   border-radius: 4px;
+//   margin-left: 16px;
+//   cursor: pointer;
+//   color: #fff;
+//   outline: none;
+//   &:hover {
+//     background-color: darkgreen;
+//   }
+// `
 
-const CopyButtonDiv = styled.div`
-  display: flex;
-  align-items: center;
-  max-width: content;
-`
+// const CopyButtonDiv = styled.div`
+//   display: flex;
+//   align-items: center;
+//   max-width: content;
+// `
 
 const ContactInfo = styled.div`
   /* add styles for the contact information section here */
@@ -150,10 +192,22 @@ export default function Blog({ jobListing }) {
   })
 
   const [copied, setCopied] = useState(false)
-  const handleCopyClick = (event) => {
-    event.preventDefault()
-    navigator.clipboard.writeText(JobPostingURLTrack)
-    setCopied(true)
+  // const handleCopyClick = (event) => {
+  //   event.preventDefault()
+  //   navigator.clipboard.writeText(JobPostingURLTrack)
+  //   setCopied(true)
+  // }
+
+  const [rotating, setRotating] = useState(false)
+
+  const handleCopyClick = async () => {
+    setRotating(true)
+    try {
+      await navigator.clipboard.writeText(JobPostingURLTrack)
+      setCopied(true)
+    } catch (error) {
+      console.error('Failed to copy text: ', error)
+    }
   }
 
   const { fields } = jobListing
@@ -182,9 +236,15 @@ export default function Blog({ jobListing }) {
       <ApplyLink href={JobPostingURLTrack} target="_blank" rel="noopener noreferrer">
         Apply Now
       </ApplyLink>{' '}
-      <CopyButton onClick={handleCopyClick}>
+      {/* <CopyButton onClick={handleCopyClick}>
         <CopyButtonDiv>
           {!copied && <CopyIcon as={copyLightIcon} />}
+          {copied ? 'Copied!' : 'Copy Link'}
+        </CopyButtonDiv>
+      </CopyButton> */}
+      <CopyButton onClick={handleCopyClick} disabled={copied}>
+        <CopyButtonDiv>
+          {copied ? null : <CopyIcon as={copyLightIcon} className={rotating ? 'rotate' : ''} />}
           {copied ? 'Copied!' : 'Copy Link'}
         </CopyButtonDiv>
       </CopyButton>
